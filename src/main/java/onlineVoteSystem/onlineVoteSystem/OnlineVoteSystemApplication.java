01d0601ded8fa0321966fda4b1129d6b98017424
Jing-Wen-Hsu <wen23479@gmail.com>
2024-12-12 18:37:21 +0800

    建置環境，設定依賴、資料庫連接與.env

---
+++ src/main/java/onlineVoteSystem/onlineVoteSystem/OnlineVoteSystemApplication.java
@@ -0,0 +1,13 @@
+package onlineVoteSystem.onlineVoteSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVoteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVoteSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onlineVoteSystem/onlineVoteSystem/OnlineVoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package onlineVoteSystem.onlineVoteSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
